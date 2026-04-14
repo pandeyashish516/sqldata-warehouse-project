063ca082ccacebca993f8caaa93f2165834c7321
pandeyashish516 <113006178+pandeyashish516@users.noreply.github.com>
2026-04-14 16:31:10 +0530

    Update datainsertion.sql

--- scripts/silver/datainsertion.sql
+++ scripts/silver/datainsertion.sql
@@ -1,1 +1,78 @@
+use DataWarehouse 
+IF OBJECT_ID('silver.crm_prd_info', 'U') IS NOT NULL
+    DROP TABLE silver.crm_prd_info;
+GO
+create table silver.crm_prd_info(
+prd_id int,
+prd_key nvarchar(50),
+prd_nm nvarchar(50),
+prd_cost nvarchar(50),
+prd_line  nvarchar(50),
+prd_start_dt datetime,
+prd_end_dt datetime,
+dwh_create_date datetime2 default getdate()
+);
+IF OBJECT_ID('silver.crm_cust_info', 'U') IS NOT NULL
+    DROP TABLE silver.crm_cust_info;
+GO
+create table silver.crm_cust_info(
+cst_id int,
+cst_key nvarchar(50),
+cst_firstname nvarchar(50),
+cst_lastname nvarchar(50),
+cst_material_status nvarchar(50),
+cst_gndr nvarchar(50),
+cst_create_date date,
+dwh_create_date datetime2 default getdate()
+);
+IF OBJECT_ID('silver.crm_sales_details', 'U') IS NOT NULL
+    DROP TABLE silver.crm_sales_details;
+GO
+create table silver.crm_sales_details(
+sls_order_num nvarchar(50),
+sls_prd_key nvarchar(50),
+sls_cust_id int,
+sls_order_dt int,
+sls_ship_dt int,
+sls_due_dt int,
+sls_sales int,
+sls_quantity int,
+sls_price int,
+dwh_create_date datetime2 default getdate()
+);
 
+IF OBJECT_ID('silver.erp_loc_a101', 'U') IS NOT NULL
+    DROP TABLE silver.erp_loc_a101;
+GO
+
+CREATE TABLE silver.erp_loc_a101 (
+    cid    NVARCHAR(50),
+    cntry  NVARCHAR(50),
+    dwh_create_date datetime2 default getdate()
+);
+GO
+
+IF OBJECT_ID('silver.erp_cust_az12', 'U') IS NOT NULL
+    DROP TABLE silver.erp_cust_az12;
+GO
+
+CREATE TABLE silver.erp_cust_az12 (
+    cid    NVARCHAR(50),
+    bdate  DATE,
+    gen    NVARCHAR(50),
+    dwh_create_date datetime2 default getdate()
+);
+go
+
+IF OBJECT_ID('silver.erp_px_cat_g1v2', 'U') IS NOT NULL
+    DROP TABLE silver.erp_px_cat_g1v2;
+GO
+
+CREATE TABLE silver.erp_px_cat_g1v2 (
+    id           NVARCHAR(50),
+    cat          NVARCHAR(50),
+    subcat       NVARCHAR(50),
+    maintenance  NVARCHAR(50),
+    dwh_create_date datetime2 default getdate()
+);
+GO
